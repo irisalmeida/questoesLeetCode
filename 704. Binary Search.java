dad626b9bf3138dd11905650e8e3fdf15a86e16f
Íris Almeida <62727312+irisalmeida@users.noreply.github.com>
2023-03-13 19:31:01 -0300

    Create 704. Binary Search.java

---
+++ 704. Binary Search.java
@@ -0,0 +1,29 @@
+//Não passa em todos os casos de teste
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int inicio = 0;
+        int fim = nums.length - 1;
+    
+    return buscaBinaria(nums, target, inicio, fim);
+}
+
+    private static int buscaBinaria(int []nums, int target,int  inicio,int fim){
+        if (inicio < fim){
+            int meio = (inicio + fim) /2;
+
+            if(nums[meio] == target){
+                return meio;
+            }
+            if(target < nums[meio]){
+                return buscaBinaria(nums,target, inicio, meio -1);
+            }
+            else{
+                return buscaBinaria(nums,target, meio +1, fim);
+            }
+        }else{
+            return -1;
+        }
+    }
+}
