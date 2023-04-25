f55a4c579d5648a36ee030a1924db19a33546c55
Íris Almeida <62727312+irisalmeida@users.noreply.github.com>
2023-04-25 15:18:41 -0300

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -1,6 +1,3 @@
-//NÃO PASSOU EM TODOS OS CASOS DE TESTE: 174 / 195 testcases passed
-//Caso em que deu erro:Input nums = [5,1,3] /target =5 /Output -1 / Expected 0
-
 class Solution {
     public int search(int[] nums, int target) {
         int inicio = 0;
@@ -9,14 +6,16 @@ class Solution {
     }
 
     private int buscaBinaria(int[] nums, int inicio, int fim, int target){
-        int meio = (inicio + fim)/2;
+
+        if(inicio > fim){
+            return -1;
+        }
+       int meio = (inicio + fim)/2;
 
         if(target == nums[meio]) {
             return meio;
-        }if(inicio > fim){
-            return -1;
-        }if(inicio <= fim) {
-            if(nums[inicio] <= target && target <= nums[meio]){
+        }if(nums[inicio] <= nums[meio]) {
+            if(target <= nums[meio] && nums[inicio] <= target ){
                 return buscaBinaria(nums, inicio, meio -1,target);
             }else{
                 return buscaBinaria(nums, meio +1, fim, target);
@@ -31,4 +30,3 @@ class Solution {
     }
 
 }   
-       
